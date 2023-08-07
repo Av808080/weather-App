bd52e7505c31d50918d2f7305007ce521dc5d6a3
Av808080 <108837984+Av808080@users.noreply.github.com>
2023-08-07 17:46:42 +0330

    Darkmode

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,8 +1,8 @@
 import { ChangeEvent, FormEvent, useState } from 'react'
 import axios from 'axios'
-//Type
-import { Data } from './Types/Data'
 import ShowData from './components/ShowData';
+
+import { Data } from './Types/Data' //Type
 
 const API_KEY = "5a4e03723d22ebc949c51818dce21b9a";
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
@@ -10,6 +10,7 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 const App = () => {
   const [data, setData] = useState({} as Data)
   const [city, setCity] = useState("")
+  const [isDarkMode , setIsDarkmode] = useState(false);
 
   const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -31,14 +32,19 @@ const App = () => {
     setData({} as Data)
   }
   return (
-    <div className='min-h-screen bg-gradient-to-br from-indigo-400 to-indigo-500 flex flex-col items-center gap-4 pt-4'>
-      <h1 className='font-bold text-3xl tracking-wider'>Weather App</h1>
+    <div className={`min-h-screen bg-gradient-to-br ${isDarkMode ? 'from-indigo-700 to-indigo-900 text-slate-50' : 'from-indigo-300 to-indigo-400'} flex flex-col items-center gap-4 pt-4`}>
+      <div className='ml-auto mr-5 text-lg font-semibold flex items-center gap-4'> darkMode :<div className={`${isDarkMode ?'bg-green-500' :'bg-slate-500'} h-6 w-16 relative rounded-xl`} onClick={()=> setIsDarkmode(d=>!d)}>
+        <span className={`${isDarkMode ? ' translate-x-9' :''} bg-slate-300 absolute top-0 left-0 h-full w-7
+        transition duration-300 rounded-xl`}></span>
+      </div>
+        </div>
+      <h1 className='font-bold text-3xl tracking-wider '>Weather App</h1>
       <form onSubmit={submitHandler} className='flex flex-col justify-center items-center gap-4'>
-        <input type='text' value={city} onChange={changeHandler} className='ring-1 ring-cyan-500 rounded-xl px-2 py-0.5 focus:outline-none focus:ring-2' placeholder='Enter A City...' />
-        <button type='submit' className='font-semibold  tracking-wider bg-orange-500 hover:bg-orange-600 transition duration-300 rounded-xl px-2 py-0.5' >Submit</button>
+        <input type='text' value={city} onChange={changeHandler} className='text-slate-900 ring-1 ring-cyan-500 rounded-xl px-2 py-0.5 focus:outline-none focus:ring-2' placeholder='Enter A City...' />
+        <button type='submit' className='font-semibold text-slate-900 tracking-wider bg-orange-500 hover:bg-orange-600 transition duration-300 rounded-xl px-2 py-0.5' >Submit</button>
       </form>
       {data.isError && <p className='text-center p-2 text-xl font-semibold text-rose-600'>{city} is Not A CityName</p>}
-      {!!data.country && <ShowData data={data} city={city} />}
+      {!!data.country && <ShowData data={data} city={city} isDarkMode={isDarkMode}/>}
     </div>
   )
 }
--- weather-app/src/components/ShowData.tsx
+++ weather-app/src/components/ShowData.tsx
@@ -6,19 +6,19 @@ import { Data } from "../Types/Data"
 type Props = {
     data: Data
     city: string
+    isDarkMode:boolean
 }
 
-
-const ShowData = (data: Props) => {
-    const { data: { country, description, icon: Image, temp } } = data;
+const ShowData = (props: Props) => {
+    const { data: { country, description, icon: Image, temp } ,city , isDarkMode} = props;
     useEffect(() => {
         AOS.init();
     }, [])
     return (
-        <div className="w-72 h-76 rounded-xl bg-indigo-300 shadow-xl shadow-indigo-500 p-3 flex flex-col items-center gap-2" data-aos='fade-up' data-aos-duration='800'>
-            <p className="text-2xl font-bold text-slate-900/95">{data.city}-<span className="bg-orange-500 mx-1 px-1 py-0.5 rounded-lg">{country} </span></p>
-            <p className="text-4xl font-bold ">{temp.toFixed(2)}&#176;C</p>
-            <p className="text-xl font-semibold tracking-wider">{description}</p>
+        <div className={`w-72 h-76 rounded-xl ${!isDarkMode ?"bg-indigo-300 shadow-indigo-500":"bg-indigo-400 shadow-indigo-600"} shadow-2xl  p-3 flex flex-col items-center gap-2`} data-aos='fade-up' data-aos-duration='800'>
+            <p className="text-2xl font-bold text-slate-900/95">{city}-<span className="bg-orange-500 mx-1 px-1 py-0.5 rounded-lg">{country} </span></p>
+            <p className="text-4xl font-bold text-slate-900 ">{temp.toFixed(2)}&#176;C</p>
+            <p className="text-xl font-semibold tracking-wider text-slate-900">{description}</p>
             <img className="w-3/5" src={Image} alt='status of weather' />
 
         </div>
